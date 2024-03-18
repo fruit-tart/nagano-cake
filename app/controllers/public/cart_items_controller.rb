a92da0124ad62b0d2553cb2b27f50c851a85bfbd
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-03-18 13:59:32 +0000

    [Fix] cart items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,6 +4,8 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    cart_item_matched_id.update(cart_item_params)
+    redirect_to action: "index"
   end
 
   def destroy
@@ -19,13 +21,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
-    if item
-      item.update(amount: item[:amount].to_i + params[:cart_item][:amount].to_i)
+    cart_item = CartItem.find_by(item_id: cart_item_params[:item_id], customer_id: current_customer.id)
+    if cart_item
+      cart_item.update(amount: cart_item[:amount] + cart_item_params[:amount].to_i)
     else
       current_customer.cart_items.create(cart_item_params)
     end
-    redirect_to action: "index"
+      redirect_to action: "index"
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   TAX_RATE = 1.1
 
   def price_include_tax(price, amount = 1)
-    ((price * TAX_RATE).floor * amount).to_s(:delimited)
+    (price * TAX_RATE).floor * amount
   end
 end
