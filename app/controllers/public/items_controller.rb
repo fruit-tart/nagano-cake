a75c2cc28ba6fca4ca3921153239baf671fc5eeb
tomoyoshimizu <154865008+tomoyoshimizu@users.noreply.github.com>
2024-03-18 05:01:11 +0000

    [Fix] items index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.where(is_active: true)
   end
 
   def show
+    @item = item_matched_id
   end
+
+  private
+    def item_matched_id
+      Item.find(params[:id])
+    end
 end
