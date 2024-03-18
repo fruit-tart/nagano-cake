35efbf0b7f728fd2ac69497f050d2b4bb6522e64
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-03-18 04:06:01 +0000

    [Add]top画面追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,10 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.all
+    @order_detail = OrderDetail.find_by(order_id: params[:order_id])
+  end
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
   end
 end
