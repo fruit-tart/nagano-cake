4cc354e910a098602afe7cef54868d7fec5cd8c0
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-03-18 14:20:09 +0000

    [Add]admin-itemのedit,show,new追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,6 +4,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
@@ -14,8 +15,24 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "successfully updated."
+      redirect_to admin_item_path(@item)
+    else
+      flash[:alert] = "error"
+      render :edit
+    end
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:image,:genre_id, :name, :introduction, :price, :is_active)
+  end
+  
 end
