310e81679cc144fa170ea5fa836b28ce72497ab9
freshtree0424 <156634421+freshtree0424@users.noreply.github.com>
2024-03-18 15:10:58 +0900

    Update homes_controller.rb

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,10 +1,5 @@
 class Admin::HomesController < ApplicationController
-  def top
-    @orders = Order.all
-    @order_detail = OrderDetail.find_by(order_id: params[:order_id])
-  end
-  
-  def order_detail_params
-    params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
+   def top
+    @orders = Order.all.order(created_at: :desc)
   end
 end
