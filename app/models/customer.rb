32b046a4e6281f9d6eba5b60bca1cefb3542842f
Haruino <harukiinoue5270@gmail.com>
2024-03-19 11:56:50 +0000

    [Add]マイページ＆会員情報編集ページ追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,11 @@ class Customer < ApplicationRecord
   has_many :addresses
   has_many :cart_items
   has_many :orders
+  
+  validates :last_name, :first_name, presence: true
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :email, presence: true
+  validates :postal_code, presence: true, length: { is: 7 }
+  validates :address, presence: true
+  validates :telephone_number, presence: true, length: { in: 1..15 }
 end
