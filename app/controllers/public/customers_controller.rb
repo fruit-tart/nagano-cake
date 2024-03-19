8071000698bf966a19fae721617fb1978c41f5d9
Haruino <harukiinoue5270@gmail.com>
2024-03-19 06:45:07 +0000

    [Start]会員関連機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,21 @@
 class Public::CustomersController < ApplicationController
+# マイページ
   def show
   end
 
+# 編集画面
   def edit
   end
   
+# 編集機能
   def update
   end
 
+# 退会確認
   def unsubscribe
   end
   
+# 退会処理
   def withdraw
   end
 end
