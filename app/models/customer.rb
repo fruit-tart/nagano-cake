19e413319bd79bc950f55ce12623ea0ef39d83d4
Haruino <harukiinoue5270@gmail.com>
2024-03-21 00:40:57 +0000

    [Add]配送先関連追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,7 +10,7 @@ class Customer < ApplicationRecord
   
   validates :last_name, :first_name, presence: true
   validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
-  validates :postal_code, presence: true, length: { is: 7 }
+  validates :postal_code, presence: true, length: { is: 7 },  format: {with: /\A\d{7}\z/}
   validates :address, presence: true
   validates :telephone_number, presence: true, length: { in: 1..15 }
 end
