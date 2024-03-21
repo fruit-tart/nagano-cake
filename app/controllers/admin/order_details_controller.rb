29415db6f0f5164bc8a420f4f64bb38a431f621d
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-03-21 13:36:05 +0000

    [Add] 注文履歴詳細ページ追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,4 +2,11 @@ class Admin::OrderDetailsController < ApplicationController
   
   def update
   end
+  
+  enum making_status: {
+    impossible: 0,
+    waiting: 1,
+    production: 2,
+    complete: 3
+  }  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,5 +4,8 @@ class Admin::OrdersController < ApplicationController
   end
   
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to order_path(@order.id)
   end
 end
