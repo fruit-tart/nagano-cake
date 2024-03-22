c2c68cb916b2379afef3db5a9b37ef672cacd1c4
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-03-22 03:00:53 +0000

    [Add]ステータス文言の追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_detail = OrderDetail.new
   end
   
   def update
@@ -8,4 +9,10 @@ class Admin::OrdersController < ApplicationController
     @order.update(order_params)
     redirect_to order_path(@order.id)
   end
+  
+  private
+  def order_params
+    params.require(:order).permit(:customer_id,:postal_code,:address,:name,:shipping_cost,:total_payment,:payment_method,:status)
+  end
+  
 end
