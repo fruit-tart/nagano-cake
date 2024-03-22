ba67af9c6310b799bfb9b252c804fbe9dd353462
Haruino <harukiinoue5270@gmail.com>
2024-03-22 03:31:36 +0000

    [Add]会員一覧ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
-    #↓会員氏名検索機能のメソッドです
+    #↓会員氏名検索機能のメソッド
     @customers = @customers.where("full_name LIKE ?", "%#{params[:search]}%") if params[:search].present?
   end
 
